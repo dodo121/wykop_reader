eb02c2cd9efd6358b6f491457fcb443df28d3430
Dominik Szromik <dominik.szromik@gmail.com>
2024-07-04 10:51:42 +0200

    add db seeds for Articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10_000.times do
+  Article.create!(title: Faker::Book.title, content: Faker::Movie.quote)
+end
